54519eb52e52b669b9823efe826f3234b749c211
akj61300 <rnlwjd0613@gmail.com>
2018-12-18 16:47:46 +0900

    랜덤 번호 생성 코드 추가

--- app/src/main/java/com/akj/lotto/MainActivity.kt
+++ app/src/main/java/com/akj/lotto/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +12,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // 랜덤으로 번호 생성 카드의 클릭 이벤트 리스너
         randomCard.setOnClickListener {
+            // ResultActivity 를 시작하는 Intent 생성
+            val intent = Intent(this, ResultActivity::class.java)
+
+            // intent 의 결과 데이터를 전달한다.
+            // int 의 리스트를 전달하므로 putIntegerArrayListExtra 를 사용한다.
+            intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
+
             // ResultActivity 를 시작하는 Intent 를 만들고 startActivity 로 실행
-            startActivity(Intent(this, ResultActivity::class.java))
+            startActivity(intent)
         }
         // 별자리로 번호 생성 카드의 클릭 이벤트 리스너
         constellationCard.setOnClickListener {
@@ -24,5 +32,37 @@ class MainActivity : AppCompatActivity() {
             // NameActivity 를 시작하는 Intent 를 만들고 startActivity 로 실행
             startActivity(Intent(this, NameActivity::class.java))
         }
+    }
+
+    /**
+     * 랜덤으로 1 ~ 45 번호중 하나의 번호를 생성하는 함수
+     */
+    fun getRandomLottoNumber(): Int {
+        // Random.nextInt 는 0 ~ 전달받은 파라미터 값 미만의 번호를 생성
+        // ex) Random().nextInt(10) 은 0 ~ 9 까지의 무작위 수를 반환
+        // 1 ~ 45 까지의 번호를 생성하려면 파라미터의 45 를 넣고 결과값의 1을 더한다.
+        return Random().nextInt(45) + 1
+    }
+
+    /**
+     * 랜덤으로 추출하여 6개의 로또 번호를 만드는 함수
+     */
+    fun getRandomLottoNumbers(): MutableList<Int> {
+        // 무작위로 생성된 로또 번호를 저장할 가변 리스트 생성
+        val lottoNumbers = mutableListOf<Int>()
+        // 6번 반복하는 for 문
+        for (i in 1..6) {
+            // 랜덤한 번호를 임시로 저장할 변수를 생성
+            var number = 0
+            do {
+                // 랜덤한 번호를 추출해 number 변수에 저장
+                number = getRandomLottoNumber()
+                // lottoNumbers 에 number 변수의 값이 없을때까지 반복
+            } while (lottoNumbers.contains(number))
+            // 이미 뽑은 리스트에 없는 번호가 나올때까지 반복했으므로 중복이 없는 상태
+            // 추출된 번호를 뽑은 리스트에 추가
+            lottoNumbers.add(number)
+        }
+        return lottoNumbers
     }
 }
